8424c64d87c013a30edfaa200e574db8c92f1a18
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-08-06 16:34:09 -0400

    feat: render icon component

--- component/main.jsx
+++ component/main.jsx
@@ -3,6 +3,7 @@ import { getLatestGames } from "../lib/metacritic";
 import { useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { GameCard } from "./GameCard";
+import { Logo } from "./Logo";
 
 export function Main() {
   const [games, setGames] = useState([]);
@@ -16,6 +17,9 @@ export function Main() {
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
       {games.length === 0 ? (
         <ActivityIndicator size="large" color="#fff" />
       ) : (
