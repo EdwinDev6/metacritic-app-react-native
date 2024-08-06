398870e327675c38ecfb32814558ab952dabd87d
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-08-06 16:58:53 -0400

    feat: animations when rendering

--- component/GameCard.jsx
+++ component/GameCard.jsx
@@ -1,4 +1,5 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { useEffect, useRef } from "react";
+import { View, Text, Image, StyleSheet, Animated } from "react-native";
 
 export function GameCard({ game }) {
   return (
@@ -11,6 +12,24 @@ export function GameCard({ game }) {
   );
 }
 
+export function AnimatedGameCard({ game, index }) {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      delay: index * 200,
+      useNativeDriver: true,
+    }).start();
+  }, [opacity, index]);
+
+  return (
+    <Animated.View style={{ opacity }}>
+      <GameCard game={game} />
+    </Animated.View>
+  );
+}
 const styles = StyleSheet.create({
   card: {
     marginBottom: 10,
